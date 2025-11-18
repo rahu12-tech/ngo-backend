8c546411cd1c951e07f97535c7b5d961285fe078
rahu12-tech <jakhar365365@gmail.com>
2025-11-18 10:36:05 +0530

    secret

--- ngobackend/settings.py
+++ ngobackend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-l!5q(nss8%8!)g4jefv^4arh0aoo%lbcok4ol7+n&lq!qb%&b1')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
